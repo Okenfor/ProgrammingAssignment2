d575f42be34476c8f67e4580a03a485cf855cdef
Pablo Andrés Pérez <pablitoperezq.colombia@gmail.com>
2014-09-21 18:08:35 -0500

    my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function calculates the inverse of a matrix, but instead of making calculus
+## on running time, it first tries to cache a calcultated one.
 
-## Write a short comment describing this function
+## This part of the excersice stores the inverse calcultated of a matrix in
+## cache memory. REturns the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse o a matrix. If it was calculated before, then returns
+## the one stored in cache memory
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
